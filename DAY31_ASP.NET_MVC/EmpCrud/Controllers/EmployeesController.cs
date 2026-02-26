c9cd38377bf3c0d245299f8d06827e61f045004e
Asad <aliasad07518@gmail.com>
2026-02-26 15:36:25 +0530

    MVC Using EF

---
+++ DAY31_ASP.NET_MVC/EmpCrud/Controllers/EmployeesController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EmpCrud.Data;
+using EmpCrud.Models;
+
+namespace EmpCrud.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Employees
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Employees.Include(e => e.Department);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employees = await _context.Employees
+                .Include(e => e.Department)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+
+            return View(employees);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
+        {
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name");
+            return View();
+        }
+
+        // POST: Employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Email,Salary,DepartmentId")] Employees employees)
+        {
+            ModelState.Remove("Department"); // Add this line too
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(employees);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name", employees.DepartmentId);
+            return View(employees);
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employees = await _context.Employees.FindAsync(id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name", employees.DepartmentId);
+            return View(employees);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Email,Salary,DepartmentId")] Employees employees)
+        {
+            if (id != employees.Id)
+            {
+                return NotFound();
+            }
+
+            ModelState.Remove("Department"); // Add this line
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(employees);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmployeesExists(employees.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DepartmentId"] = new SelectList(_context.Departments, "Id", "Name", employees.DepartmentId);
+            return View(employees);
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var employees = await _context.Employees
+                .Include(e => e.Department)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (employees == null)
+            {
+                return NotFound();
+            }
+
+            return View(employees);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var employees = await _context.Employees.FindAsync(id);
+            if (employees != null)
+            {
+                _context.Employees.Remove(employees);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmployeesExists(int id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DAY31_ASP.NET_MVC/EmpCrud/Models/Restaurants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmpCrud.Models;
+
+public partial class Restaurants
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string City { get; set; } = null!;
+}
