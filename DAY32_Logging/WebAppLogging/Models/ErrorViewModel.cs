f42185a9e759b117418a7f8ca50024258de2c0f2
Asad <aliasad07518@gmail.com>
2026-02-28 12:24:41 +0530

    Serilog

---
+++ DAY32_Logging/WebAppLogging/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppLogging.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
